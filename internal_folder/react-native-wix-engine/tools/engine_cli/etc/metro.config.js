d1056dd66254883e79b80d5a44a2548d85152ef1
Douglas Morais <mr.douglasmorais23@gmail.com>
2024-12-29 06:57:29 -0300

    chore: First commit

---
+++ internal_folder/react-native-wix-engine/tools/engine_cli/etc/metro.config.js
@@ -0,0 +1,26 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const config = {
+  projectRoot: path.resolve(__dirname, '../../../../../'),
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    enableGlobalPackages: true,
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
